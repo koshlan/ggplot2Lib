e23fe536bca4ef8c1a1fc427f257548b5a6d4208
koshlan <koshlan@gmail.com>
2015-02-26 19:01:43 -0800

    added markup very tentative

--- First.R
+++ First.R
@@ -1,7 +1,21 @@
 # First Script
 require(ggplot2)
-head(mtcars)
-p <- ggplot2::ggplot(mtcars, aes(mpg, hp, colour = gear))
-p <- p + geom_point()
-summary(p)
-p
+# df <- data.frame(x=c(3,1,5), y = c(2,4,6), label = c("a","b","c"))
+# library(ggplot2) 
+# geometry <- c(geom_point(), geom_bar(),geom_line(), geom_area(),geom_path(), geom_text(), geom_polygon())
+# pl = list()
+# foo <- function(x){
+#   p <- ggplot(df, aes(x,y, label=label)) 
+#   p <- p + x
+#   return(p)
+#   }
+# for (i in 1:length(geometry)){
+#   str(geometry[i])
+#   g <- foo(geometry[i])
+#   invisible(pl[[i]] <- g)
+# pl[[2]]
+# pl[[3]]
+# pl[[4]]
+# pl[[5]]
+# pl[[6]]
+# pl[[7]]      
